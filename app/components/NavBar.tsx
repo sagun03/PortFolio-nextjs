70af80bdb6c7f3e0589a569a412b7252a07bfd8b
sagun03 <sagunsaluja13@gmail.com>
2023-12-21 15:28:25 -0500

    added pic for avtar and added search bar from mui v5

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -13,8 +13,62 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import ImportantDevicesIcon from '@mui/icons-material/ImportantDevices';
 import { useState } from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import { InputBase } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const CustomAppBar = styled(AppBar)(({ theme }) => ({
+  '& .MuiContainer-root': {
+    paddingRight: '12px',
+    paddingLeft: '12px'
+  },
+  '& .MuiToolbar-root': {
+    minHeight: '64px', // Center align items in the toolbar
+  },
+}));
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common?.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common?.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }: { theme: any}) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 function NavBar() {
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
@@ -34,12 +88,30 @@ function NavBar() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+  const styles = {
+    root: {
+      backgroundColor: 'transparent',
+      boxShadow: 'none',
+    },
+  };
 
   return (
     <nav className="border-b border-gray-700 h-16">
-    <AppBar position="static" color='transparent'>
+    <CustomAppBar position="static" color='transparent'>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
+        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
           <ImportantDevicesIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
@@ -48,6 +120,7 @@ function NavBar() {
             href="/"
             sx={{
               mr: 2,
+              flexGrow: 1,
               display: { xs: 'none', md: 'flex' },
               fontFamily: 'monospace',
               fontWeight: 700,
@@ -76,24 +149,33 @@ function NavBar() {
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
-              mr: 2,
+              mr: 1,
               display: { xs: 'flex', md: 'none' },
               flexGrow: 1,
               fontFamily: 'monospace',
               fontWeight: 700,
-              letterSpacing: '.3rem',
+              letterSpacing: '.1rem',
               color: 'inherit',
               textDecoration: 'none',
             }}
           >
             PORTFOLIO
           </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="sagun pic" src="../../public/pic.png" />
+                <Avatar alt="sagun pic" src="/pic.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
@@ -121,7 +203,7 @@ function NavBar() {
           </Box>
         </Toolbar>
       </Container>
-    </AppBar>
+    </CustomAppBar>
     </nav>
   );
 }
