d14944ec2be1046dec737ca8dc273500c6044c61
sagun03 <sagunsaluja13@gmail.com>
2024-01-06 17:23:12 -0500

    added emojies for categories

--- app/skills/components/const.ts
+++ app/skills/components/const.ts
@@ -1,31 +1,35 @@
 export const skillsData = [
     {
       category: 'Programming Technologies',
+      emoji: '💻',
       skills: [
-        { name: 'Pyhthon', rating: 3.5 },
         { name: 'React', rating: 4.5 },
         { name: 'JavaScript', rating: 4.5 },
+        { name: 'TypeScript', rating: 4 },
+        { name: 'Python', rating: 3.5 },
         { name: 'Node JS', rating: 3 },
         { name: 'Express', rating: 3 },
-        { name: 'TypeScript', rating: 4 },
+        { name: 'Angular', rating: 2.5 },
       ],
     },
     {
       category: 'Libraries',
+      emoji: '📚',
       skills: [
         { name: 'Recoil', rating: 4.5 },
-        { name: 'Redux', rating: 4 },
-        { name: 'Apollo', rating: 3.5 },
-        { name: 'Graph QL', rating: 4 },
         { name: 'Material UI', rating: 4.5 },
+        { name: 'Redux', rating: 4 },
+        { name: 'Graph QL', rating: 4 },
+        { name: 'TailWind CSS', rating: 4 },
         { name: 'Jest', rating: 4 },
         { name: 'RTL', rating: 3.5 },
+        { name: 'Apollo', rating: 3.5 },
         { name: 'React-Table', rating: 3.5 },
-        { name: 'TailWind CSS', rating: 4 },
       ],
     },
     {
       category: 'Scripting Language',
+      emoji: '🌐',
       skills: [
         { name: 'HTML5', rating: 4.5 },
         { name: 'CSS', rating: 4 },
@@ -35,6 +39,7 @@ export const skillsData = [
     },
     {
       category: 'Database',
+      emoji: '🛢️',
       skills: [
         { name: 'MongoDB', rating: 3.5 },
         { name: 'sql', rating: 3}
@@ -42,18 +47,20 @@ export const skillsData = [
     },
     {
       category: 'Tools',
+      emoji: '🛠️',
       skills: [
         { name: 'GitHub', rating: 4.5 },
+        { name: 'Aglie', rating: 4.5 },
         { name: 'Firebase', rating: 3.5},
-        { name: 'Aglie', rating: 4.5 }
       ]
     },
     {
       category: 'Wireframing',
+      emoji: '🖍️',
       skills: [
         { name: 'Draw.io', rating: 4.5 },
+        { name: 'Microsoft PowerPoint', rating: 4.5 },
         { name: 'Figma', rating: 3},
-        { name: 'Microsoft PowerPoint', rating: 4.5 }
       ]
     }
   ];
--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -5,11 +5,11 @@ import { skillsData } from "./components/const";
 export default function Skills() {
   return (
     <div className="bg-gray-900 p-6 rounded-lg shadow-md text-white">
-      <h2 className="text-3xl font-semibold mb-4">Technologies and Skills</h2>
-      <p>Skillscape 🌟: The Artillery of Expertise 🚀</p>
+      <h2 className="text-3xl  mb-4">Technologies and Skills</h2>
+      <p className="font-light">Skillscape 🌟: The Artillery of Expertise 🚀</p>
       {skillsData.map((category, index) => (
-        <div key={index} className="mb-6">
-          <h3 className="text-xl flex items-center font-semibold mb-2">{category.category}</h3>
+        <div key={index} className="mb-6 mt-6">
+          <h3 className="text-xl flex mb-6">{category.emoji} {category.category}</h3>
           <div className="flex gap-6 flex-wrap">
           <SkillCard skills={category.skills} />
           </div>
