db75c5801d6f11934fafee8c9b8ee0a2157a97d3
sagun03 <sagunsaluja13@gmail.com>
2024-01-08 17:29:12 -0500

    added projects page

---
+++ app/projects/consts.ts
@@ -0,0 +1,25 @@
+export const projects = [{
+    name: 'Omnia Food',
+    github: 'https://github.com/sagun03/Omnia-Food',
+    live: 'https://omnia-food.netlify.app/',
+    tech: ['HTML', 'CSS'],
+    desc: 'A healthy meal delivered to your door, every single day',
+    img: '/html.png'
+},
+{
+    name: 'Utilities Hub',
+    github: 'https://github.com/sagun03/Utilities-Hub',
+    live: 'https://utilities-hub.netlify.app/home',
+    tech: ['React', 'Mui5', 'Styled Components', ],
+    desc: 'Welcome to Utilities Hub, your central resource for all your utility needs!',
+    img: '/html.png'
+},
+{
+    name: 'Ecommerce Jk Total Washing Solutions',
+    github: 'https://github.com/sagun03/react-shopping-ui',
+    live: 'https://jktotalwashingsolutions.netlify.app/',
+    tech: ['React', 'Swiper JS', 'Firebase', 'Redux'],
+    desc: 'One Stop Solution for all HouseHould cleaning and Washing',
+    img: '/html.png'
+}
+]
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
-
-export default function Projects() {
+import React from "react";
+import { projects } from "./consts";
+import ProjectCard from "./components/ProjectCard";
+const Projects = () => {
   return (
-    <main className="flex">
-      Projects
+    <main className="container mx-auto px-8 py-12">
+      <h2 className="text-4xl font-bold text-center mb-12">Projects</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        {projects.map(({ name, live, github, tech, desc, img }) => (
+          <ProjectCard
+            name={name}
+            live={live}
+            github={github}
+            tech={tech}
+            desc={desc}
+            img={img}
+          />
+        ))}
+      </div>
     </main>
   );
-}
+};
+
+export default Projects;
