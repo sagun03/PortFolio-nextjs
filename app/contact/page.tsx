a493c00b02ec5ff5b0d0f3aa9f25029c77b6958a
sagun03 <sagunsaluja13@gmail.com>
2024-01-09 17:02:58 -0500

    added styling for contact page as well

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactForm from "./components/ContactForm";
 
 export default function ContactMe() {
   return (
-    <main className="container h-about-me-scroll flex justify-center items-center mx-auto px-8 bg-gray-900 p-6 rounded-lg shadow-md">
+    <main className="container h-full flex justify-center items-center mx-auto px-8 bg-gray-900 p-6 rounded-lg shadow-md">
      <ContactForm />
     </main>
   );
