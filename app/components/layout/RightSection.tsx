17020aa89c2a30c154952b3debc561af9b62188f
sagun03 <sagunsaluja13@gmail.com>
2024-02-07 19:21:01 -0500

    updated right section for mobile view

--- app/components/layout/RightSection.tsx
+++ app/components/layout/RightSection.tsx
@@ -1,31 +1,31 @@
-import HomeRepairServiceIcon from "@mui/icons-material/HomeRepairService";
 import SocialMediaLinks from "./SocialMediaLinks";
-import Image from "next/image";
-import ContributionGraph from "../contributionGrpah/ContributionGraph";
+
 const RightSection = () => {
   return (
     <main className="flex-custom1 p-6">
       <section className="py-1">
         <div className="flex items-center flex-col">
+          <div className="flex justify-center w-full sm:flex-col">
           <div className="relative">
             <img
               src="/pic3.jpeg"
               alt="Profile Circle"
-              className="rounded-full w-36 h-36 md:w-52 md:h-52 lg:w-72 lg:h-72 drop-shadow-md"
+              className="rounded-full w-36 max-h-28 sm:w-40 sm:max-h-40 md:w-52 md:max-h-52 lg:w-72 lg:max-h-72 drop-shadow-md"
             />
           </div>
-          <div className="mt-4 justify-items-start w-full">
-            <h3 className="text-2xl font-semibold">Sagun Saluja</h3>
+          <div className="p-2 sm:p-0 flex flex-col text-center sm:text-start sm:block justify-center sm:mt-4 sm:justify-items-start  w-full">
+            <h2 className="text-3xl font-semibold sm:text-2xl">Sagun Saluja</h2>
             <p className="text-gray-600 font-semibold text-xl">
               Software Developer
             </p>
           </div>
-          <div className="mt-1 justify-items-start w-full">
-            <p className="text-sm">
+          </div>
+          <div className="mt-6 sm:mt-1 sm:justify-items-start w-full">
+            <p className="text-base text-center sm:text-sm sm:text-start">
               👋 I&apos;m a passionate Software Developer with 5+ years of
               dedicated experience deeply enthusiastic about React (However, this portfolio is built in next js ✨)
             </p>
-            <div className="py-3 mt-2">
+            <div className="py-3 mt-2 hidden sm:block">
             {/* <ContributionGraph /> */}
             <SocialMediaLinks />
             </div>
