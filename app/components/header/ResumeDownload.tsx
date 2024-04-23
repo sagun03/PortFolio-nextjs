63d17b643fed080a3bc744b7e9d8923fa407eeee
sagun03 <sagunsaluja13@gmail.com>
2024-04-23 17:38:12 -0400

    fix blank resume issue

--- app/components/header/ResumeDownload.tsx
+++ app/components/header/ResumeDownload.tsx
@@ -3,14 +3,24 @@ import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 import IconButton from "@mui/material/IconButton";
 
 const ResumeDownload = ({ isMobile }: { isMobile: boolean }) => {
-  const handleDownload = () => {
-    const resumeUrl = "/resume.pdf";
-    const link = document.createElement("a");
-    link.href = resumeUrl;
-    link.download = "Sagun Saluja Resume.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const handleDownload = async () => {
+    try {
+      const resumeUrl = "/sagun_resume.pdf";
+      const response = await fetch(resumeUrl);
+      if (!response.ok) {
+        throw new Error("Failed to fetch resume");
+      }
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "Sagun_Saluja_Resume.pdf";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error("Error downloading resume:", error);
+    }
   };
 
   return (
